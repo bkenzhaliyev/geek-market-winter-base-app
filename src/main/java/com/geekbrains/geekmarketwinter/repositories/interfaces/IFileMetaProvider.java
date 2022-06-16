fc1e046c62695afa52311063b4ad43912e278252
bkenzhaliyev <bkenzhaliyev@gmail.com>
2022-06-16 21:37:16 +0600

    hw 8

---
+++ src/main/java/com/geekbrains/geekmarketwinter/repositories/interfaces/IFileMetaProvider.java
@@ -0,0 +1,22 @@
+package com.geekbrains.geekmarketwinter.repositories.interfaces;
+
+import com.geekbrains.geekmarketwinter.entites.FileMetaDTO;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public interface IFileMetaProvider {
+
+    String checkFileExists(UUID fileHash);
+    String checkFileNameExists(UUID fileHash, String fileName);
+
+    /**
+     * Сохранить метаданные файла
+     *
+     */
+    void saveFileMeta(UUID Hash, String fileName, int sybType);
+
+    void deleteFileMeta(UUID Hash, String fileName);
+
+    Collection<FileMetaDTO> getMetaFiles(int subtype);
+}
